3fc732f9b4933a793056a1ff9fb4c5de2d79891d
CatgirlJustin <123514212+CatgirlJustin@users.noreply.github.com>
2026-01-09 15:09:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from PyQt6.QtWidgets import QMainWindow, QApplication
 from PyQt6.QtCore import Qt, QTimer
 from PyQt6.QtGui import QIcon
-import sys, os
-import time
-import threading
+import sys, os, time, threading
 import cv2, pyautogui, numpy, keyboard
 import json
 import ctypes
@@ -13,74 +11,51 @@ from modules.GlobalVariables import *
 from modules.SettingsWindow import SettingsWindow
 from modules.LogsWindow import LogsWindow
 
-# Vetex, this message is for you. If I get caught, I think I deserve to be praised...
-
-# Ha-ha
-# while I was writing the program I spent 10 days and caught only two sunken treasures (i caught 5000+ fish)
-# but i got Expert Angler title, hah
-
+# ------------------------
+# Utility Functions
+# ------------------------
 def locateImage(img, threshold: float):
     screenshot = pyautogui.screenshot()
-    screenshot = numpy.array(screenshot)
-    screenshot = cv2.cvtColor(screenshot, cv2.COLOR_RGB2BGR)
-    image = numpy.uint8(img)
-    result = cv2.matchTemplate(screenshot, image, cv2.TM_CCOEFF_NORMED)
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    screenshot = cv2.cvtColor(numpy.array(screenshot), cv2.COLOR_RGB2BGR)
+    result = cv2.matchTemplate(screenshot, img, cv2.TM_CCOEFF_NORMED)
+    _, max_val, _, max_loc = cv2.minMaxLoc(result)
     if max_val >= threshold:
-        return (max_loc[0], max_loc[1])
+        return max_loc
     return None
 
 def getLastSize() -> list[int]:
-    file = open("DB.json", "r")
-    data = json.loads(file.read())
+    with open("DB.json", "r") as file:
+        data = json.load(file)
     size = data["screenSize"][0]
     return [size["width"], size["height"]]
 
 def changeLastSize(newSize: list[int]) -> None:
     with open("DB.json", "r") as file:
-        data = json.loads(file.read())
+        data = json.load(file)
         settings = data["settings"][0]
     newDBobject = {
         "settings": [settings],
-        "screenSize": [{
-            "width": newSize[0],
-            "height": newSize[1]
-        }]}
+        "screenSize": [{"width": newSize[0], "height": newSize[1]}]
+    }
     with open('DB.json', 'w') as file:
         json.dump(newDBobject, file)
 
 def changeImageSize(path: str, monitorSize: list[int], lastUsedSize: list[int]) -> None:
     img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
-    imgW = img.shape[1]
-    imgH = img.shape[0]
-    ratioW = monitorSize[0] * 100 / lastUsedSize[0] * 0.01
-    ratioH = monitorSize[1] * 100 / lastUsedSize[1] * 0.01
-    targetSize = (int(ratioW*imgW)+4, int(ratioH*imgH)+1)
+    ratioW = monitorSize[0] / lastUsedSize[0]
+    ratioH = monitorSize[1] / lastUsedSize[1]
+    targetSize = (int(img.shape[1]*ratioW)+4, int(img.shape[0]*ratioH)+1)
     outIMG = cv2.resize(img, targetSize, cv2.INTER_LINEAR)
     cv2.imwrite(path, outIMG)
 
+# ------------------------
+# Main Window
+# ------------------------
 class MainWindow(QMainWindow):
-    title: str = "title"
-    isFishing: bool = False
-    tryCatchFish: bool = False
-    startThisTry: float = 0
-    maxTimeForWait: int = 70
-    startFishingTimer: float = 0
-    startCheckMealTimer: float = 0
-    checkMealTimer: int = 0
-    startCheckPotionTimer: float = 0
-    checkPotionTimer: int = 0
-    shouldStopFishing: bool = False
-    fishCount: int = 0
-
     def __init__(self, title: str):
         super().__init__()
-
-        self.title: str = title
+        self.title = title
         self.icon = APP_ICON
-        
-        self.setObjectName("MainWindow")
-
         self.setWindowFlags(Qt.WindowType.FramelessWindowHint | Qt.WindowType.WindowStaysOnTopHint)
         self.setWindowTitle(self.title)
         self.setWindowIcon(QIcon(self.icon))
@@ -88,144 +63,193 @@ class MainWindow(QMainWindow):
         self.move((screenSize.width() // 2) - (self.width() // 2), 0)
         self.setStyleSheet(CSS)
 
-        self.fishingThread = threading.Thread(target = self.fishing)
+        # ------------------------
+        # State Variables
+        # ------------------------
+        self.isFishing = False
+        self.tryCatchFish = False
+        self.shouldStopFishing = False
+        self.fishCount = 0
+        self.startFishingTimer = 0
+        self.startCheckMealTimer = 0
+        self.startCheckPotionTimer = 0
+        self.maxTimeForWait = 70
 
+        # ------------------------
+        # Windows
+        # ------------------------
         self.settingsWindow = SettingsWindow(self)
         self.logsWindow = LogsWindow(self)
 
+        # ------------------------
+        # UI Buttons
+        # ------------------------
         self.btn_start = Button(self, "START", 2, 2, 75, 26, "btn_standart", self.startFishing)
         self.btn_start.setToolTip("Start fishing")
-        btn_close = Button(self, EXIT_ICON, self.width() - 28, 2, 26, 26, "btn_red", self.closeEvent)
-        btn_close.setToolTip("Close window")
-        btn_settings = Button(self, SETTING_ICON, self.width() - 56, 2, 26, 26, "btn_standart", self.openSettings)
-        btn_settings.setToolTip("Settings window")
-        btn_logs = Button(self, LOGS_ICON, self.width() - 84, 2, 26, 26, "btn_standart", self.openLogsWindow)
-        btn_logs.setToolTip("History window")
+        Button(self, EXIT_ICON, self.width()-28, 2, 26, 26, "btn_red", self.closeEvent).setToolTip("Close window")
+        Button(self, SETTING_ICON, self.width()-56, 2, 26, 26, "btn_standart", self.openSettings).setToolTip("Settings window")
+        Button(self, LOGS_ICON, self.width()-84, 2, 26, 26, "btn_standart", self.openLogsWindow).setToolTip("History window")
         self.__countLabel = Label(self, 79, 2, 135, 26, "", f"Caught: {self.fishCount}")
 
+        # ------------------------
+        # Timer
+        # ------------------------
         self.ShouldStopFishingTimer = QTimer(self)
         self.ShouldStopFishingTimer.setInterval(500)
         self.ShouldStopFishingTimer.timeout.connect(self.checkShouldStopFishing)
         self.ShouldStopFishingTimer.start()
 
-    def openSettings(self) -> None:
-        if self.settingsWindow.isVisible():
-            self.settingsWindow.hide()
-        else:
-            self.settingsWindow.show()
+        # ------------------------
+        # Fishing Thread
+        # ------------------------
+        self.fishingThread = threading.Thread(target=self.fishing, daemon=True)
+        self.fishingThread.start()
 
-    def openLogsWindow(self) -> None:
-        if self.logsWindow.isVisible():
-            self.logsWindow.hide()
-        else:
-            self.logsWindow.show()
+    # ------------------------
+    # UI Methods
+    # ------------------------
+    def openSettings(self):
+        self.settingsWindow.setVisible(not self.settingsWindow.isVisible())
 
-    def startFishing(self) -> None:
-        self.isFishing = not (self.isFishing)
-        if self.isFishing and not self.shouldStopFishing:
-            self.btn_start.setObjectName("btn_red")
-            self.btn_start.setText("STOP")
-            self.btn_start.setToolTip("Stop fishing")
+    def openLogsWindow(self):
+        self.logsWindow.setVisible(not self.logsWindow.isVisible())
+
+    def startFishing(self):
+        if not self.isFishing:
             self.isFishing = True
             self.startFishingTimer = time.time()
             self.startCheckMealTimer = time.time()
             self.startCheckPotionTimer = time.time()
             self.logsWindow.logs.append([time.localtime(), "start"])
+            self.btn_start.setObjectName("btn_red")
+            self.btn_start.setText("STOP")
+            self.btn_start.setToolTip("Stop fishing")
             self.setStyleSheet(CSS)
         else:
             self.shouldStopFishing = True
 
-    def checkShouldStopFishing(self) -> None:
+    def checkShouldStopFishing(self):
         if self.shouldStopFishing:
+            self.isFishing = False
+            self.tryCatchFish = False
+            self.shouldStopFishing = False
             self.btn_start.setObjectName("btn_standart")
             self.btn_start.setText("START")
             self.btn_start.setToolTip("Start fishing")
-            self.isFishing = False
-            self.tryCatchFish = False
             self.logsWindow.logs.append([time.localtime(), "stop"])
             self.setStyleSheet(CSS)
-            self.shouldStopFishing = False
-        self.ShouldStopFishingTimer.start()
 
-    def closeEvent(self, event) -> None:
+    def closeEvent(self, event=None):
         self.settingsWindow.close()
         self.logsWindow.close()
         self.close()
 
-    def fishing(self) -> None:
+    # ------------------------
+    # Fishing Logic
+    # ------------------------
+    def fishing(self):
         while self.isVisible():
-            if self.isFishing:
-                self.timeForWait = time.time() - self.startFishingTimer
-                self.checkMealTimer = time.time() - self.startCheckMealTimer
-                self.checkPotionTimer = time.time() - self.startCheckPotionTimer
-                if locateImage(IMG_START, 0.7):
-                    self.tryCatchFish = True
-                    self.startThisTry = time.time()
-                    while self.tryCatchFish:
-                        pyautogui.click(button = "left")
-                        timeForThisTry = time.time() - self.startThisTry
-                        if (locateImage(IMG_FISH, 0.8) or locateImage(IMG_JUNK, 0.8)) and (timeForThisTry <= self.settingsWindow.timeForTry):
-                            self.endTry("fish")
-                            self.addFishCount()
-                        else: pyautogui.click(button = "left")
-                        if locateImage(IMG_TREASURE, 0.7) and (timeForThisTry <= self.settingsWindow.timeForTry) and self.tryCatchFish:
-                            self.endTry("treasure")
-                            self.addFishCount()
-                        else: pyautogui.click(button = "left")
-                        if locateImage(IMG_SUNKEN, 0.8) and (timeForThisTry <= self.settingsWindow.timeForTry) and self.tryCatchFish:
-                            self.endTry("sunken")
-                            self.addFishCount()
-                        else: pyautogui.click(button = "left")
-                        if timeForThisTry > self.settingsWindow.timeForTry:
-                            time.sleep(1)
-                            self.endTry("timeError")
-                        else: pyautogui.click(button = "left")
-
-                elif self.timeForWait >= self.maxTimeForWait:
-                    if locateImage(IMG_DISCONNECTED, 0.8): self.shouldStopFishing = True
-                    else: self.endTry("timeError")
-
-                elif (self.checkMealTimer >= self.settingsWindow.mealTimer) and (self.settingsWindow.useMeal):
-                    keyboard.press_and_release(f"{self.settingsWindow.mealKey}")
-                    pyautogui.click(button = "left")
-                    time.sleep(0.75)
-                    keyboard.press_and_release(f"{self.settingsWindow.rodKey}")
-                    pyautogui.click(button = "left")
-                    self.logsWindow.logs.append([time.localtime(), "consumeMeal"])
-                    self.startFishingTimer = time.time()
-                    self.startCheckMealTimer = time.time()
-                elif (self.checkPotionTimer >= self.settingsWindow.potionTimer) and (self.settingsWindow.usePotion):
-                    keyboard.press_and_release(f"{self.settingsWindow.potionKey}")
-                    if self.settingsWindow.potionKey != "e":
-                        pyautogui.click(button = "left")
-                        time.sleep(0.75)
-                        keyboard.press_and_release(f"{self.settingsWindow.rodKey}")
-                        pyautogui.click(button = "left")
-                    self.logsWindow.logs.append([time.localtime(), "consumePotion"])
-                    self.startFishingTimer = time.time()
-                    self.startCheckPotionTimer = time.time()
-
+            if not self.isFishing:
                 time.sleep(0.25)
+                continue
 
-    def endTry(self, log: str) -> None:
-        rodKey = f"{self.settingsWindow.rodKey}"
+            elapsed = time.time() - self.startFishingTimer
+            mealElapsed = time.time() - self.startCheckMealTimer
+            potionElapsed = time.time() - self.startCheckPotionTimer
+
+            # Detect start of fishing
+            if locateImage(IMG_START, 0.7) and not self.tryCatchFish:
+                self.tryCatchFish = True
+                self.startThisTry = time.time()
+
+            # Handle catch
+            if self.tryCatchFish:
+                self.handleCatch()
+
+            # Timeout
+            if elapsed >= self.maxTimeForWait:
+                if locateImage(IMG_DISCONNECTED, 0.8):
+                    self.shouldStopFishing = True
+                else:
+                    self.endTry("timeError")
+
+            # Consumables
+            if mealElapsed >= self.settingsWindow.mealTimer and self.settingsWindow.useMeal:
+                self.consumeMeal()
+            if potionElapsed >= self.settingsWindow.potionTimer and self.settingsWindow.usePotion:
+                self.consumePotion()
+
+            time.sleep(0.25)
+
+    # ------------------------
+    # Handle Catch
+    # ------------------------
+    def handleCatch(self):
+        timeForThisTry = time.time() - self.startThisTry
+        caughtSomething = False
+
+        for img, logName in [(IMG_FISH, "fish"), (IMG_JUNK, "fish"),
+                             (IMG_TREASURE, "treasure"), (IMG_SUNKEN, "sunken")]:
+            if locateImage(img, 0.8) and timeForThisTry <= self.settingsWindow.timeForTry:
+                self.endTry(logName)
+                if logName in ["fish", "treasure", "sunken"]:
+                    self.addFishCount()
+                caughtSomething = True
+                break
+
+        # Timeout catch
+        if timeForThisTry > self.settingsWindow.timeForTry and not caughtSomething:
+            self.endTry("timeError")
+
+        # Only click once to reel in
+        if not caughtSomething:
+            pyautogui.click(button="left")
+
+    # ------------------------
+    # End Try
+    # ------------------------
+    def endTry(self, log: str):
+        if not self.tryCatchFish:
+            return
         self.tryCatchFish = False
         self.startFishingTimer = time.time()
         self.logsWindow.logs.append([time.localtime(), log])
-        time.sleep(0.2)
-        keyboard.press_and_release(rodKey)
-        keyboard.press_and_release(rodKey)
-        time.sleep(0.2)
-        pyautogui.click(button = "left")
+        pyautogui.click(button="left")  # Only reel in; never touch rod key
 
-    def addFishCount(self) -> None:
+    # ------------------------
+    # Fish Count
+    # ------------------------
+    def addFishCount(self):
         self.fishCount += 1
         self.__countLabel.setText(f"Caught: {self.fishCount}")
 
-    def resetFishCount(self) -> None:
+    def resetFishCount(self):
         self.fishCount = 0
         self.__countLabel.setText(f"Caught: {self.fishCount}")
 
+    # ------------------------
+    # Consumables
+    # ------------------------
+    def consumeMeal(self):
+        keyboard.press_and_release(self.settingsWindow.mealKey)
+        time.sleep(0.75)
+        pyautogui.click(button="left")  # only click, do NOT touch rod
+        self.logsWindow.logs.append([time.localtime(), "consumeMeal"])
+        self.startFishingTimer = time.time()
+        self.startCheckMealTimer = time.time()
+
+    def consumePotion(self):
+        keyboard.press_and_release(self.settingsWindow.potionKey)
+        time.sleep(0.75)
+        pyautogui.click(button="left")  # only click, do NOT touch rod
+        self.logsWindow.logs.append([time.localtime(), "consumePotion"])
+        self.startFishingTimer = time.time()
+        self.startCheckPotionTimer = time.time()
+
+
+# ------------------------
+# Startup
+# ------------------------
 if __name__ == "__main__":
     user32 = ctypes.windll.user32
     user32.SetProcessDPIAware()
@@ -233,6 +257,7 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     screenSize = app.primaryScreen().geometry()
 
+    # Resize images if screen changed
     allImagesPath = [
         Rf'{os.path.abspath(os.path.dirname(sys.argv[0]))}/images/forScript/start.png',
         Rf'{os.path.abspath(os.path.dirname(sys.argv[0]))}/images/forScript/fish.png',
@@ -245,8 +270,8 @@ if __name__ == "__main__":
     actualScreenSize = [user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)]
     lastUsedSize = getLastSize()
     if lastUsedSize[0] != actualScreenSize[0]:
-        for i in allImagesPath:
-            changeImageSize(i, actualScreenSize, lastUsedSize)
+        for path in allImagesPath:
+            changeImageSize(path, actualScreenSize, lastUsedSize)
         changeLastSize(actualScreenSize)
 
     IMG_START = cv2.imread(allImagesPath[0])
@@ -258,7 +283,4 @@ if __name__ == "__main__":
 
     window = MainWindow("Auto fishing")
     window.show()
-    window.fishingThread.start()
     sys.exit(app.exec())
-
-#  pyinstaller -w -F -i"images\icons\APP_ICON.ico" -n "auto fishing" main.py
